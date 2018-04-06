1ad8594c5714f368258457b46d711e377e7b038e
William Campbell Crocker <williamcrocker1992@gmail.com>
2018-04-05 20:07:09 -0700

    first

---
+++ server.js
@@ -0,0 +1,61 @@
+
+var request = require("request");
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var path = require("path");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "test" }));
+
+app.set("view engine", "handlebars");
+
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./test.html"));
+});
+
+
+app.get("/login", function(req, res) {
+   
+   res.sendFile(path.join(__dirname, "./login.html"));
+});
+
+app.get("/create", function(req, res) {
+    res.sendFile(path.join(__dirname, "./create.html"));
+});
+
+app.get("/contactUs", function(req, res) {
+    res.sendFile(path.join(__dirname, "./contactUs.html"));
+});
+
+app.get("/aboutUs", function(req, res) {
+    res.sendFile(path.join(__dirname, "./aboutUs.html"));
+});
+
+app.get("/trainerLogin", function(req, res) {
+    res.sendFile(path.join(__dirname, "./trainerLogin.html"));
+});
+
+app.get("/trainerCreate", function(req, res) {
+    res.sendFile(path.join(__dirname, "./trainerCreate.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+});
