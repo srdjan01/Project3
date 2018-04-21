9781700210f2b7eca82300d8f0808240872a12c9
Julian Sandersius <30510599+JSandersius@users.noreply.github.com>
2018-04-21 09:56:42 -0700

    changes to file arrangment, only old server.js file is left in -toBeIntegrated

--- ToBeIntegrated/server.js
+++ ToBeIntegrated/server.js
@@ -1,39 +1,44 @@
+//integrated this into server.js in root file
+
+
 // *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
+// var express = require("express");  got it 
+// var bodyParser = require("body-parser"); got it 
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+// var app = express(); got it 
+// var PORT = process.env.PORT || 8080; got it 
 
 // Requiring our models for syncing
-var db = require("./models");
+// var db = require("./models");  //added this to server
 
 // Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false })); got it
 // parse application/json
-app.use(bodyParser.json());
+
+// app.use(bodyParser.json()); got it 
 
 // Static directory
-app.use(express.static("public"));
+// app.use(express.static("public")); got it 
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);  added these to server.js
+// require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-});
+// db.sequelize.sync({ force: true }).then(function() {   added this to server
+
+//   app.listen(PORT, function() {                 got it
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
